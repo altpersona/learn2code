ff27cc9835548658e4c6b6bb6c8b7d0aed5e37a6
altpersona <61986933+altpersona@users.noreply.github.com>
2020-03-21 13:48:06 -0400

    Create tutor.py
    Python3 version of the tutor script, works better than the bash version.

---
+++ tutor.py
@@ -0,0 +1,26 @@
+# a script to assist building habits.
+import sys
+import os
+
+fo = open(sys.argv[1])
+print ("Name of the file: "), fo.name
+
+for line in fo.readlines():
+    os.system('clear')    
+    Newline = line.strip()  
+    if line.startswith('#'):
+        continue
+    if line.startswith('//'):
+        continue
+    if line.startswith('/*'):
+        continue
+    if line.startswith('*/'):
+        continue
+    print (Newline)
+    MyString = input()
+    while MyString != Newline:
+        print (Newline)
+        MyString = input()
+    
+# Close opend file
+fo.close()
